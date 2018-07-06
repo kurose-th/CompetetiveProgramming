97e21401951b91686c2af20a2d89c7ca0dcd59f3
kurose <krs1218@gmail.com>
2018-07-06 23:34:33 +0900

    ATC001A

--- AtCoder/Past/ATC001A.cpp
+++ AtCoder/Past/ATC001A.cpp
@@ -15,12 +15,45 @@ using namespace std;
 #define mp make_pair
 #define all(x) (x).begin(), (x).end()
 
+
+int H, W;
+char maze[510][510];
+bool reached[510][510];
+
+void dfs(int x, int y){
+
+    if(x<0 || y<0 || x >= W || y >= H || maze[x][y] == '#') return;
+    if(reached[x][y]) return;
+    reached[x][y] = true;
+
+    dfs(x-1, y);
+    dfs(x+1, y);
+    dfs(x, y-1);
+    dfs(x, y+1);
+
+}
+
 // A - 深さ優先探索
 int main(){
-    int H, W;
     cin >> H >> W;
 
-    int ans = 0;
+    int start[2];
+    int goal[2];
+    rep(i, 0, H){
+        rep(j, 0, W){
+            cin >> maze[j][i];
+            if(maze[j][i] == 's'){
+                start[0] = j;
+                start[1] = i;
+            }
+            if(maze[j][i] == 'g'){
+                goal[0] = j;
+                goal[1] = i;
+            }
+        }
+    }
+    dfs(start[0], start[1]);
+    string ans = (reached[goal[0]][goal[1]]) ? "Yes" : "No";
     cout << ans << endl;
     return 0;
 }
