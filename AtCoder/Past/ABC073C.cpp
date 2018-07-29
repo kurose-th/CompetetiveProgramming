d6a9ecfdaa6678882e5e54482148b2f7a339047a
kurose_laptop <krs1218@gmail.com>
2018-07-29 22:17:58 +0900

    rep for iterator

--- AtCoder/Past/ABC073C.cpp
+++ AtCoder/Past/ABC073C.cpp
@@ -13,18 +13,27 @@ using namespace std;
 typedef long long ll;
 #define rep(i, a, n) for (int i=a;i<n;++i)
 #define repeq(i, a, n) for(int i=a;i<=n;++i)
-#define repi(m, itr) for (auto itr = m.begin();itr != m.end();++itr)
 #define per(i, a, n) for (int i=n-1; i>=a;--i)
 #define pb push_back
 #define mp make_pair
 #define all(x) (x).begin(), (x).end()
 
-// 
+// C - Write and Erase
 int N;
+map<int, int> A;
 int ans = 0;
 
 int main(){
     cin >> N;
+    rep(i, 0, N){
+        int tmp;
+        cin >> tmp;
+        ++A[tmp];
+    }
+
+    for(auto itr = A.begin();itr != A.end();++itr){
+        if(itr->second %2 == 1) ++ans;
+    }
 
     cout << ans << endl;
     return 0;
--- AtCoder/Past/ABC082C.cpp
+++ AtCoder/Past/ABC082C.cpp
@@ -13,18 +13,31 @@ using namespace std;
 typedef long long ll;
 #define rep(i, a, n) for (int i=a;i<n;++i)
 #define repeq(i, a, n) for(int i=a;i<=n;++i)
-#define repi(m, itr) for (auto itr = m.begin();itr != m.end();++itr)
 #define per(i, a, n) for (int i=n-1; i>=a;--i)
+#define repi(m, itr) for (auto itr = m.begin();itr != m.end();++itr)
 #define pb push_back
 #define mp make_pair
 #define all(x) (x).begin(), (x).end()
 
-// 
+// C - Good Sequence
 int N;
+map<int, int> m;
 int ans = 0;
 
 int main(){
     cin >> N;
+    rep(i, 0, N){
+        int tmp;
+        cin >> tmp;
+        ++m[tmp];
+    }
+    repi(m, itr){
+        if(itr->first <= itr->second){
+            ans += itr->second - itr->first;
+        }else{
+            ans += itr->second;
+        }
+    }
 
     cout << ans << endl;
     return 0;
